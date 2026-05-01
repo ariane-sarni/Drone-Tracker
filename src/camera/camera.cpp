df075fc35f75642424fc01814d59eb969a75700a
ariane-sarni <arianesarni@gmail.com>
2026-04-30 18:49:55 -0700

    formatted

--- src/camera/camera.cpp
+++ src/camera/camera.cpp
@@ -4,72 +4,69 @@
 using namespace libcamera;
 
 void throw_error(int ret, const std::string &message) {
-    std::cerr << "FATAL ERROR: " << message << " (Error code: " << ret << (")");
-    std::exit(EXIT_FAILURE);
+  std::cerr << "FATAL ERROR: " << message << " (Error code: " << ret << (")");
+  std::exit(EXIT_FAILURE);
 }
 
-
-std::unique_ptr<CameraManager> createManager(){
-    std::unique_ptr<CameraManager> cm = std::make_unique<CameraManager>();
-    int ret = cm->start();
-    if (ret < 0){
-        throw_error(ret, "Failed to initialize camera manager.");
-    }
-    return cm;
+std::unique_ptr<CameraManager> createManager() {
+  std::unique_ptr<CameraManager> cm = std::make_unique<CameraManager>();
+  int ret = cm->start();
+  if (ret < 0) {
+    throw_error(ret, "Failed to initialize camera manager.");
+  }
+  return cm;
 }
 
-
-std::vector<std::shared_ptr<Camera>> getCameraList(CameraManager &Manager){
-    auto cameras = Manager.cameras();
-    if (cameras.empty()) {
-        throw_error(0, "Failed to find a camera. Did you initialize the camera manager properly?");
-        Manager.stop();
-    }
-    return cameras;
+std::vector<std::shared_ptr<Camera>> getCameraList(CameraManager &Manager) {
+  auto cameras = Manager.cameras();
+  if (cameras.empty()) {
+    throw_error(0, "Failed to find a camera. Did you initialize the camera "
+                   "manager properly?");
+    Manager.stop();
+  }
+  return cameras;
 }
 
-std::string getCameraID(Camera &Cam) {
-    return Cam.id();
+std::string getCameraID(Camera &Cam) { return Cam.id(); }
+
+std::shared_ptr<Camera> obtainCamera(CameraManager &Manager,
+                                     std::string cameraID) {
+  auto camera = Manager.get(cameraID);
+  if (!camera) {
+    throw_error(0, "Failed to get camera. Either Camera Manager wasn't "
+                   "initialized, or no cameras are plugged in.");
+  }
+  return camera;
 }
 
-std::shared_ptr<Camera> obtainCamera(CameraManager &Manager, std::string cameraID){
-    auto camera = Manager.get(cameraID);
-    if (!camera){
-        throw_error(0, "Failed to get camera. Either Camera Manager wasn't initialized, or no cameras are plugged in.");
-    }
-    return camera;
+void acquireCamera(Camera &Cam) {
+  int ret = Cam.acquire();
+  if (ret < 0) {
+    throw_error(0, "Failed to acquire camera. Was it unplugged?");
+  }
 }
-
-void acquireCamera(Camera &Cam){
-    int ret = Cam.acquire();
-    if (ret < 0) {
-        throw_error(0, "Failed to acquire camera. Was it unplugged?");
-    }
-} 
 
 void cameraTest() {
-    // 1. Create camera manager, intialize it.
-    // 2. Get camera list. 
-    // 3. Get Camera id, acquire camera. 
-    // 4. Set up config
-    auto cameraManager = createManager();
-    auto cameraList = getCameraList(*cameraManager);
-    std::string cameraID = getCameraID(*cameraList[0]);
+  // 1. Create camera manager, intialize it.
+  // 2. Get camera list.
+  // 3. Get Camera id, acquire camera.
+  // 4. Set up config
+  auto cameraManager = createManager();
+  auto cameraList = getCameraList(*cameraManager);
+  std::string cameraID = getCameraID(*cameraList[0]);
 
-    auto camera = obtainCamera(*cameraManager, cameraID);
-    acquireCamera(*camera);
+  auto camera = obtainCamera(*cameraManager, cameraID);
+  acquireCamera(*camera);
 
-    // Now we can move onto configuration
-    // Probably look like the follownig: 
-    // makeConfig, returns configuration object. 
-    // Maybe a change config / apply config settings? 
-    // validateConfig, takes configuration (after changing)  
-    // setCameraConfig, set a cameras configuration 
-
-
+  // Now we can move onto configuration
+  // Probably look like the follownig:
+  // makeConfig, returns configuration object.
+  // Maybe a change config / apply config settings?
+  // validateConfig, takes configuration (after changing)
+  // setCameraConfig, set a cameras configuration
 }
 
 int main() {
-    cameraTest();
-    return 0;
+  cameraTest();
+  return 0;
 }
--- src/camera/camera.h
+++ src/camera/camera.h
@@ -1,70 +1,67 @@
-#pragma once 
+#pragma once
 
-#include <iostream>
-#include <memory>
-#include <thread>
-#include <iomanip>
-#include <libcamera/libcamera.h>
-#include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <libcamera/formats.h>
-#include <mutex>
 #include <cstring>
-#include <sys/mman.h>
+#include <iomanip>
+#include <iostream>
 #include <jpeglib.h>
+#include <libcamera/formats.h>
+#include <libcamera/libcamera.h>
+#include <memory>
+#include <mutex>
 #include <stdexcept>
-
+#include <stdio.h>
+#include <sys/mman.h>
+#include <thread>
 
 // Okay. Things to do.
 // Initialize Camera Manager.
-//  I could list all camera ids. Not neccesary. 
-// Initialize cameras. 
-// Acquire camera. 
+//  I could list all camera ids. Not neccesary.
+// Initialize cameras.
+// Acquire camera.
 // Create configuration for camera, change its settings, validate them.
-// Allocate frames. 
-// Set up camera. Connect it and start it. 
-// Request from camera. 
-// Clean up. 
+// Allocate frames.
+// Set up camera. Connect it and start it.
+// Request from camera.
+// Clean up.
 
 // According to good value semantics for C++, following should happen:
 // Modern C++, objects are usually cheap to move.
 // Want data to generally flow in one direction in functions (out)
 // RAII - Object should be fully functional moment its constructed.
-// Example of that is for manager for camera list. 
+// Example of that is for manager for camera list.
 // As of C++17, returning by value isnt that slow honestly.
 // Basically we are following Value-Based design.
 // Treat complex objects the same way you'd treat an int.
-// Piece of data that can be created, returned, or passed around without worring about pointers/dynamic memory
-
-
+// Piece of data that can be created, returned, or passed around without worring
+// about pointers/dynamic memory
 
 // Needs integration with SDL. Work out later, for now, simplify camera logic.,
-// Minimum ~6 functions. 
+// Minimum ~6 functions.
 
 // Done!
 // Creates manager and starts it.
-// Args: None 
+// Args: None
 // Returns: Unique pointer to camera manager
 std::unique_ptr<libcamera::CameraManager> createManager();
 
-// Creates a camera object with CameraID type from the Camera Manager, and grabs it.
-// Args: Initialized Manager, Camera ID
-// Returns: Pointer to Camera object
-std::shared_ptr<libcamera::Camera> obtainCamera(libcamera::CameraManager &Manager, std::string cameraID);
-
+// Creates a camera object with CameraID type from the Camera Manager, and grabs
+// it. Args: Initialized Manager, Camera ID Returns: Pointer to Camera object
+std::shared_ptr<libcamera::Camera>
+obtainCamera(libcamera::CameraManager &Manager, std::string cameraID);
 
 // Initialize actual camera.
 // Args: Reference to Camera
-// Rets: Camera ID 
+// Rets: Camera ID
 std::string getCameraID(libcamera::Camera &Cam);
 
-
 // Done!
-// Makes a list of cameras based off camera manager. 
-// Args: Reference to Camera Manager 
+// Makes a list of cameras based off camera manager.
+// Args: Reference to Camera Manager
 // Rets: Vector of shared pointers all being cameras.
-std::vector<std::shared_ptr<libcamera::Camera>> getCameraList(libcamera::CameraManager &Manager);
+std::vector<std::shared_ptr<libcamera::Camera>>
+getCameraList(libcamera::CameraManager &Manager);
 
 // Done!
 // Acquires camera.
@@ -75,4 +72,4 @@ void acquireCamera(libcamera::Camera &Cam);
 // Function to pass in configuration files
 // Args: Reference to Camera
 // Rets: Fail or success
- int configureCamera();
+int configureCamera();
