9bfa42c52d5ed3cfe896307d6d2a2baeef0d5021
ariane-sarni <arianesarni@gmail.com>
2026-05-04 20:10:06 -0700

    Started on detection

---
+++ src/detection/detect.h
@@ -0,0 +1,15 @@
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <jpeglib.h>
+#include <libcamera/formats.h>
+#include <libcamera/libcamera.h>
+#include <memory>
+#include <mutex>
+#include <stdexcept>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <thread>
+#include <onnxruntime/onnxruntime_cxx_api.h>
+
+// Using onnx run time and YOLO trained data!
